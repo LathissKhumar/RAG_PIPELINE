44c54d6691e5d20a1a9bea570dd5a2e8a383e463
Manolisha R <rmanolisha.csbs2024@citchennai.net>
2025-12-06 14:08:21 +0530

    Added /ask endpoint and RAG pipeline

---
+++ app/routers/ask.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+from app.utils.rag_pipeline import rag_query
+
+router = APIRouter()
+
+@router.get("/ask")
+def ask_q(question: str):
+    answer = rag_query(question)
+    return {"question": question, "answer": answer}
